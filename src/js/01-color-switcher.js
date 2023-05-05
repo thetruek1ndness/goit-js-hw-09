794f70f42afcc46547c48c8146a14b13845db2f1
thetruek1ndness <andrii.ptrnk@gmail.com>
2023-05-05 21:17:18 +0300

    fist task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
 
+let intervalId = 0;
+stopBtnRef.setAttribute('disabled', true);
+
+startBtnRef.addEventListener('click', onStartBtnClick);
+stopBtnRef.addEventListener('click', onStopBtnClick);
+
+
+function onStartBtnClick() {
+  stopBtnRef.removeAttribute('disabled');
+  startBtnRef.setAttribute('disabled', true);
+  intervalId = setInterval(bodyColorChange, 1000);
+}
+
+function bodyColorChange(){
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onStopBtnClick(){
+  if(intervalId === 0){
+    return;
+  }
+  startBtnRef.removeAttribute('disabled');
+  clearInterval(intervalId);
+  stopBtnRef.setAttribute('disabled', true);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
